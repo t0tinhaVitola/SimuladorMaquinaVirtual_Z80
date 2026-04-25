c9fa844bf105e82650ee6c85b9f7e70ff785047f
Otavio Bender Vitola <otaviobvoficial2005@gmail.com>
2026-04-25 18:54:19 -0300

    Adicionei boa parte dos opcode's no conversor de mnemonicos.

--- src/Main.java
+++ src/Main.java
@@ -8,40 +8,5 @@ public class Main {
         }
        
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /* uns testes ai, nada importante :p 
-        LinkedList something = new LinkedList();
-
-        something.addNode(2);
-        something.addNode(3);
-        something.addNode(100);
-
-        something.printAll();
-
-        something.removeFirstOccurance(3);
-        something.printAll();
-
-        Stack somethingStack = new Stack( 10 );
-        somethingStack.printTop();
-        somethingStack.push(5);
-        somethingStack.printTop();
-        somethingStack.push(3);
-        somethingStack.push(-1);
-        somethingStack.push(9);
-        somethingStack.printTop(); */
-
     }
 }
--- src/cpu/Translator.java
+++ src/cpu/Translator.java
@@ -12,27 +12,203 @@ public class Translator {
     }
 
     private void initializefixedInstructionsTable(){
-        fixedInstructionsTable.put("NOPE", 0x00 );
-        fixedInstructionsTable.put("HALT", 0x76 );
+        fixedInstructionsTable.put( "NOP", 0x00 );
+        fixedInstructionsTable.put( "HALT", 0x76 );
+        fixedInstructionsTable.put( "JP", 0xC3 );
+        fixedInstructionsTable.put( "JR", 0x18 );
+        fixedInstructionsTable.put( "CALL", 0xCD );
+        fixedInstructionsTable.put( "RET", 0xC9 );
     }   
 
     private void initializeRegisterTable(){  
-        registerTable.put("AC", 0b111 );
-        registerTable.put("R1", 0b000 );
-        registerTable.put("R2", 0b001 );
-        registerTable.put("R3", 0b010 );
-        registerTable.put("R4", 0b011 );
-        registerTable.put("R5", 0b100 );
-        registerTable.put("R6", 0b101 );
+        registerTable.put("A", 0b111 );
+        registerTable.put("B", 0b000 );
+        registerTable.put("C", 0b001 );
+        registerTable.put("D", 0b010 );
+        registerTable.put("E", 0b011 );
+        registerTable.put("H", 0b100 );
+        registerTable.put("L", 0b101 );
     }
 
-    public ArrayList<Byte> mnemonicsToBinary(String mnemonic){
-        String[] tokens = mnemonic.split("\\s+");
-        switch(tokens[0]){
-            case "LD": 
-            
-            break;
+    public ArrayList<Byte> mnemonicsToBinary( String mnemonic ){
+        String[] tokens = mnemonic.split( "\\s+", 2 );
+        ArrayList<Byte> somethingUHD = new ArrayList<>();
+        String[] temp;
+        int opcode = 0;
+        switch( tokens[0] ){
+            case "LD": { // Faltam ainda 2 instruções de load que eu não fiz pq não entendi oq elas querem dizer. "LD r, (HL)"" e "LD (HL), r"
+                temp = tokens[1].split( "," );
+
+                int destiny = registerTable.get( temp[0].trim() );
+                int origin;
+
+                if ( !isNumber( temp[1].trim() ) ) {
+                    origin = registerTable.get( temp[1].trim() ); // LD r, r'
+                } else {
+                    opcode = 0b00000000 + (destiny << 3 ) + 0b110; // LD r, n
+                    somethingUHD.add( ( byte ) opcode );
+                    somethingUHD.add( ( byte ) Integer.parseInt( temp[1].trim() ) );
+                    break;
+                }
+
+                opcode = 0b01000000 + ( destiny << 3 ) + origin;
+                somethingUHD.add( ( byte ) opcode );
+                break;
+            }
+            case "ADD": {
+                temp = tokens[1].split( "," );
+                
+                if ( !temp[0].trim().equals( "A" ) )
+                    break;
+                
+                int operand = registerTable.get( temp[1].trim() );
+
+                opcode = 0b10000000 + operand;
+                somethingUHD.add( ( byte ) opcode );
+                break;
+            }
+            case "SUB": {
+                temp = tokens[1].split( "," );
+                if ( temp.length != 1 ) {
+                    throw new IllegalArgumentException("SUB não está no formato esperado! (SUB r)");
+                }
+
+                int operand = registerTable.get( temp[0].trim() );
+
+                opcode = 0b10010000 + operand;
+                somethingUHD.add( ( byte ) opcode );
+                break;
+            }
+            case "INC": {
+                temp = tokens[1].split( "," );
+                if ( temp.length != 1 ) {
+                    throw new IllegalArgumentException("INC não está no formato esperado! (INC r)");
+                }
+
+                int operand = registerTable.get( temp[0].trim() );
+
+                opcode = 0b00000100 + (operand << 3);
+                somethingUHD.add( ( byte ) opcode );
+                break;
+            }
+            case "DEC": {
+                temp = tokens[1].split( "," );
+                if ( temp.length != 1 ) {
+                    throw new IllegalArgumentException("DEC não está no formato esperado! (DEC r)");
+                }
+
+                int operand = registerTable.get( temp[0].trim() );
+
+                opcode = 0b00000101 + (operand << 3);
+                somethingUHD.add( ( byte ) opcode );
+                break;
+            }
+            case "AND": {
+                temp = tokens[1].split( "," );
+                if ( temp.length != 1 ) {
+                    throw new IllegalArgumentException("AND não está no formato esperado! (AND r)");
+                }
+
+                int operand = registerTable.get( temp[0].trim() );
+
+                opcode = 0b10100000 + operand;
+                somethingUHD.add( ( byte ) opcode );
+                break;
+            }
+            case "OR": {
+                temp = tokens[1].split( "," );
+                if ( temp.length != 1 ) {
+                    throw new IllegalArgumentException("OR não está no formato esperado! (OR r)");
+                }
+
+                int operand = registerTable.get( temp[0].trim() );
+
+                opcode = 0b10110000 + operand;
+                somethingUHD.add( ( byte ) opcode );
+                break;
+            }
+            case "XOR": {
+                temp = tokens[1].split( "," );
+                if ( temp.length != 1 ) {
+                    throw new IllegalArgumentException("XOR não está no formato esperado! (XOR r)");
+                }
+
+                int operand = registerTable.get( temp[0].trim() );
+
+                opcode = 0b10101000 + operand;
+                somethingUHD.add( ( byte ) opcode );
+                break;
+            }
+            case "CP": {
+                temp = tokens[1].split( "," );
+                if ( temp.length != 1 ) {
+                    throw new IllegalArgumentException("CP não está no formato esperado! (CP r)");
+                }
+
+                int operand = registerTable.get( temp[0].trim() );
+
+                opcode = 0b10111000 + operand;
+                somethingUHD.add( ( byte ) opcode );
+                break;
+            }
+            case "JP": {
+                somethingUHD.add( ( byte ) 0xC3 );
+                //Aqui só está salvando o byte do opcode, não soube como lidar com o endereço para onde irá saltar.
+                break;
+            }
+            case "JR": {
+                temp = tokens[1].split( "$" );
+
+                if ( !isNumber( temp[1] ) )
+                    throw new IllegalArgumentException("JR não está no formato esperado! (JR offset)");
+                
+                int offset = Integer.parseInt( temp[1].trim() );
+                if ( offset > 127 || offset < -128 ) {
+                    throw new IllegalArgumentException("O offset está fora dos limites.");
+                }
+
+                somethingUHD.add( ( byte ) 0x18 );
+                somethingUHD.add( ( byte ) offset );
+                break;
+            }
+
+            case "CALL": {
+                somethingUHD.add( ( byte ) 0xCD );
+                //Aqui só está salvando o byte do opcode, não soube como lidar com o endereço para onde irá saltar.
+                break;
+            }
+            case "RET": {
+                somethingUHD.add( ( byte ) 0xC9 );
+                //Aqui só está salvando o byte do opcode, não soube como lidar com o endereço para onde irá saltar.
+                break;
+            }
+            case "PUSH": {
+                //Não soube aplicar a lógica de par de registradores
+                break;
+            }
+            case "POP": {
+                //Não soube aplicar a lógica de par de registradores
+                break;
+            }
+            case "NOP": {
+                somethingUHD.add( ( byte ) 0x00 );
+                break;
+            }
+            case "HALT": {
+                somethingUHD.add( ( byte ) 0x76 );
+                break;
+            }
         }
-        return null; //so tirando erro do compilador, enchendo o saco
+
+        return somethingUHD; 
+    }
+
+    public boolean isNumber( String a ) {
+        try {
+            Integer.parseInt( a );
+            return true;
+        } catch ( NumberFormatException e ) {
+            return false;
+        }
     }
 }
