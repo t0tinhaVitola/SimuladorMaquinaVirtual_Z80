274cd259306d98203cfeeb91580afa5057542758
Lucas <lucaslemes471@gmail.com>
2026-05-02 14:48:55 -0300

    Criando run e trocando o nome dos regs Z80 para letras

--- src/cpu/Z80.java
+++ src/cpu/Z80.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
 
-
-
 public class Z80{
-    byte AC, R1, R2, R3, FLAG, R4, R5, R6;
+    byte A, B, C, D, E, H, L, F;
     int PC, SP, IX, IY;
     final int MEMSIZE = 65536;
     byte MEM[] = new byte[MEMSIZE];
@@ -17,14 +15,14 @@ public class Z80{
     public Z80(String fileAdress) throws Exception{
         //SP, IX, IY?
         PC   = 0;
-        AC   = 0;
-        R1   = 0;
-        R2   = 0;
-        R3   = 0;
-        R4   = 0;
-        R5   = 0;
-        R6   = 0;
-        FLAG = 0;
+        A    = 0;
+        B    = 0;
+        C    = 0;
+        D    = 0;
+        E    = 0;
+        H    = 0;
+        L    = 0;
+        F    = 0;
         
         fillMemoryWithInstructions(fileAdress);
     }
@@ -45,6 +43,47 @@ public class Z80{
                         throw new RuntimeException("Text file surpassed the max instruction count");
                     }
                 }
+            }
+        }
+    }
+
+    byte getRegister(int code){
+        switch(code){
+            case 0b000: return B;
+            case 0b001: return C;
+            case 0b010: return D;
+            case 0b011: return E;
+            case 0b100: return H;
+            case 0b101: return L;
+            case 0b111: return A;
+            default: throw new RuntimeException("Invalid Register");
+        }
+    }
+
+    public void run(){
+        while (true){
+            int opcode = Byte.toUnsignedInt(MEM[PC++]);
+            
+            //instrucoes fixas 
+            switch(opcode){
+                case 0x00: //NOP
+                    break;
+                
+                case 0x76: //HALT 
+                    return;
+
+                case 0x3E: //LD A, n 
+                    A = MEM[PC++];
+                    break;
+            }
+
+            //instrucoes genericas
+
+            //ADD A,r 
+            if((opcode & 0b11111000) == 0b10000000){
+                int regCode = opcode & 0b0000111;
+                byte value = getRegister(regCode); 
+                A += value;
             }
         }
     }
