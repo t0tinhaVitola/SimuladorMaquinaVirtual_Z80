d639ff09fbe49a0cd2b9eb1cfb3f0da9503cb76c
Lucas <lucaslemes471@gmail.com>
2026-05-02 15:44:26 -0300

    Adicionando a funcao run e um display dos registradores

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@ import cpu.Z80;
 public class Main {
     public static void main(String[] args){ 
         try{
+             if(args.length == 0){
+                System.out.println("Uso: java Main arquivo.asm");
+                return;
+             }
              Z80 z80 = new Z80(args[0]);
+
+             z80.run();
+
+             z80.display_registers();   
         }catch(Exception e){
             System.out.println("Erro na leitura do arquivo");
         }
